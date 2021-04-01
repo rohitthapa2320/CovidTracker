771e0422cf2c7742df50d87f16c0185e0f607cfa
rohitthapa2320 <rohitthapa2362000@gmail.com>
2021-04-01 23:29:00 +0530

    Added API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Cards, Chart , CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
 
-const App = () => {
-  return(
-    <h1>App</h1>
-  );
+class App extends React.Component{
+  async componentDidMount(){
+    const data= await fetchData();
+    console.log(data);
+  }
+  render(){
+    return(
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
