f9157b73052e7fc5dfb205192467625053bc9c2c
rohitthapa2320 <rohitthapa2362000@gmail.com>
2021-04-02 11:36:28 +0530

    Final Commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,47 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData= async () => {
+export const fetchData= async (country) => {
+  let changedUrl=url;
+
+  if(country){
+    changedUrl=`${url}/countries/${country}`
+  }
   try {
 
-    const response= await axios.get(url);
+    const {data: {confirmed, recovered, deaths, lastUpdate}}= await axios.get(changedUrl);
 
-    return response;
+    const modifiedData ={confirmed, recovered, deaths, lastUpdate};
+
+    return modifiedData;
     
   } catch (error) {
     console.log(error);
+  }
+}
+
+export const fetchDailyData= async() => {
+try {
+  const {data} = await axios.get(`${url}/daily`);
+  
+  const modifiedData= data.map((dailyData) => ({
+    confirmed: dailyData.confirmed.total,
+    deaths: dailyData.deaths.total,
+    date: dailyData.reportDate
+  }))
+
+  return modifiedData;
+  
+} catch (err) {
+  console.log(err);
+}
+}
+
+export const fetchCountries = async () => {
+  try {
+    const {data: {countries}} = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);   
   }
 }
